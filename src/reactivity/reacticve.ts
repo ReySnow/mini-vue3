f9afd022b555be4993230fe89c69ffd59cf99e20
ReySnow <1286653145@qq.com>
2022-05-02 23:28:33 +0800

    effect.readonly getter setter

--- src/reactivity/reacticve.ts
+++ src/reactivity/reacticve.ts
@@ -1,19 +1,14 @@
-import { track, trigger } from "./effect"
+import { mutableHandlers, readonlyHandlers } from "./baseHandlers"
 
 export function reactive(raw) {
+    return createActiviteObject(raw, mutableHandlers)
+}
+
+export function readonly(raw) {
+    return createActiviteObject(raw, readonlyHandlers)
+}
+
+function createActiviteObject(raw: any, baseHandlers) {
     // 创建响应式对象
-    return new Proxy(raw, {
-        get(target, key) {
-            const res = Reflect.get(target, key)
-            // 依赖收集
-            track(target, key)
-            return res
-        },
-        set(target, key, value) {
-            const res = Reflect.set(target, key, value)
-            // 触发依赖
-            trigger(target, key)
-            return res
-        }
-    })
+    return new Proxy(raw, baseHandlers)
 }
